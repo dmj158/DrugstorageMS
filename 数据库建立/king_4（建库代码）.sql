06be242494cc01f058e1f75242ca8dabc9cc6513
HIS <niu280783872@qq.com>
2017-10-18 20:57:57 +0800

    经修改

--- 数据库建立/king_4（建库代码）.sql
+++ 数据库建立/king_4（建库代码）.sql
@@ -3,7 +3,7 @@ IF DB_ID('DBdrugstorage') IS NOT NULL
 CREATE DATABASE DBdrugstorage
 		ON 
 			(NAME='DataFile_1'
-			,FILENAME='C:\DBdrugstorage\DateFile_1.mdf')
+			,FILENAME='C:\DBdrugstorage\DataFile_1.mdf')
 		LOG ON 
 			(NAME='LogFile_1'
 			,FILENAME='C:\DBdrugstorage\LogFile_1.ldf');
@@ -12,11 +12,11 @@ IF OBJECT_ID ('tb_MedicineType') IS NOT NULL
 		DROP TABLE tb_MedicineType ;
 GO 
 CREATE TABLE tb_MedicineType
-	(No
+	(TypeNo
 		INT 
 		NOT NULL 
 		CONSTRAINT pk_MedicineType_No
-			PRIMARY KEY(No)
+			PRIMARY KEY(TypeNo)
 	,Type 
 		VARCHAR(50)
 		NOT NULL);
@@ -25,12 +25,12 @@ IF OBJECT_ID ('tb_Medicine') IS NOT NULL
 		DROP TABLE tb_Medicine ;
 GO 
 CREATE TABLE tb_Medicine
-	(No 
+	(MedicineNo 
 		INT 
 		NOT NULL 
 		CONSTRAINT pk_Medicine_No
-			PRIMARY KEY(No)
-	,Name
+			PRIMARY KEY(MedicineNo)
+	,MedicineName
 		VARCHAR(50)
 		NOT NULL
 	,TypeNo
@@ -51,26 +51,22 @@ CREATE TABLE tb_Medicine
 	,DoseUnit
 		VARCHAR(10)
 		NOT NULL
-	,Dt_Produce 
-		DATE 
-		NOT NULL 
-	,Guarantee
+	, 
+	Guarantee
 		INT 
 		NOT NULL 
-	,Firm
-	    VARCHAR(50)
-	    NOT NULL);
+	);
 		
 IF OBJECT_ID ('tb_User') IS NOT NULL 
 		DROP TABLE tb_User ;
 GO 
 CREATE TABLE tb_User
-	(No
+	(UserNo
 		INT 
 		NOT NULL 
 		CONSTRAINT pk_User_No
-			PRIMARY KEY(No)
-	,Name
+			PRIMARY KEY(UserNo)
+	,UserName
 		VARCHAR(20)
 		NOT NULL 
 	,PassCode
@@ -81,7 +77,7 @@ IF OBJECT_ID ('tb_InWarehouse') IS NOT NULL
 		DROP TABLE tb_InWarehouse;
 GO 
 CREATE TABLE tb_InWarehouse
-	(No
+	(InNo
 		INT 
 		NOT NULL 
 		CONSTRAINT pk_InWarehouse_No
@@ -94,17 +90,17 @@ CREATE TABLE tb_InWarehouse
 		NOT NULL 
 		CONSTRAINT fk_InWarehouse_UserNo
 			FOREIGN KEY(UserNo)
-			REFERENCES tb_User(No)
-	,MedicineNo
+			REFERENCES tb_User(UserNo)
+	,MedicineName
 		INT 
 		NOT NULL 
 		CONSTRAINT fk_InWarehouse_MedicineNo
-			FOREIGN KEY(MedicineNo)
-			REFERENCES tb_MEdicine(No)
-	,Amount 
+			FOREIGN KEY(MedicineName)
+			REFERENCES tb_MEdicine(MedicineNo)
+	,InAmount 
 		INT 
 		NOT NULL 
-	,Price
+	,InPrice
 	    FLOAT 
 	    NOT NULL );
 	    
@@ -112,11 +108,11 @@ IF OBJECT_ID ('tb_Pharmacy') IS NOT NULL
 		DROP TABLE tb_Pharmacy;
 GO 
 CREATE TABLE tb_Pharmacy		
-	(No 
+	(PhNo 
 		INT 
 		NOT NULL 
 		CONSTRAINT pk_Pharmacy_No
-			PRIMARY KEY(No)
+			PRIMARY KEY(PhNo)
 	,Type 
 		VARCHAR(10)
 		NOT NULL );
@@ -125,24 +121,24 @@ IF OBJECT_ID ('tb_OutWarehouse') IS NOT NULL
 		DROP TABLE tb_OutWarehouse;
 GO 
 CREATE TABLE tb_OutWarehouse
-	(No
+	(OutNo
 		INT 
 		NOT NULL 
 		CONSTRAINT pk_Pharmary_No
-			PRIMARY KEY(No)
+			PRIMARY KEY(OutNo)
 	,PharmacyNo
 		INT 
 		NOT NULL 
 		CONSTRAINT fk_OutWarehouse_PharmacyNo
 			FOREIGN KEY(PharmacyNo)
-			REFERENCES tb_Pharmacy(No)	
+			REFERENCES tb_Pharmacy(PhNo)	
 	,MedicineNo
 		INT 
 		NOT NULL 
 	    CONSTRAINT fk_OutWarehouse_MedicineNo
 			FOREIGN KEY(MedicineNo)
 	        REFERENCES tb_Medicine(No)
-	,Amount 
+	,OutAmount 
 		INT 
 		NOT NULL 
 	,OutDate 
@@ -153,17 +149,15 @@ IF OBJECT_ID ('tb_Checker') IS NOT NULL
 		DROP TABLE tb_Checker;
 GO 
 CREATE TABLE tb_Checker
-	(No 
+	(ChNo 
 		INT 
 		NOT NULL
 		CONSTRAINT pk_Checker_No
-			PRIMARY KEY(No) 
-	,Name 
+			PRIMARY KEY(ChNo) 
+	,ChName 
 		VARCHAR(20)
 		NOT NULL
-	,PassCode
-		VARBINARY(MAX)
-		NOT NULL);
+	);
 		
 IF OBJECT_ID ('tb_Stock') IS NOT NULL 
 		DROP TABLE tb_Stock;
@@ -179,7 +173,7 @@ CREATE TABLE tb_Stock
 		NOT NULL 
 		CONSTRAINT fk_Stock_MedicineNo
 			FOREIGN KEY(MedicineNo)
-			REFERENCES tb_Medicine(No)
+			REFERENCES tb_Medicine(MedicineNo)
 	,StockAmount
 		INT 
 		NOT NULL 
@@ -192,12 +186,40 @@ CREATE TABLE tb_Stock
 	,OutPrice 
 		Float 
 		NOT NULL 
-	,CheckerNo
+	,ChNo
 		INT 
 		NOT NULL 
-		CONSTRAINT fk_Stock_CheckerNo
-			FOREIGN KEY(CheckerNo)
-			REFERENCES tb_Checker(No));
+		CONSTRAINT fk_Stock_ChNo
+			FOREIGN KEY(ChNo)
+			REFERENCES tb_Checker(ChNo));
+IF OBJECT_ID('tb_Priceadjust') IS NOT NULL
+	DROP TABLE tb_priceadjust;
+create TABLE tb_priceadjust
+	(PrNo
+		INT
+		NOT NULL
+		CONSTRAINT pk_priceadjust_PrNo
+		PRIMARY KEY(PrNo)
+	,MedicineNo
+		INT
+		NOT NULL
+		CONSTRAINT fk_Priceadjust_MedicineNo
+			FOREIGN KEY(MedicineNo)
+			REFERENCES tb_Medicine(MedicineNo)
+	,UserNo
+		INT 
+		NOT NULL
+		CONSTRAINT fk_Priceadjust_UserNo
+			FOREIGN KEY(UserNo)
+			REFERENCES tb_User(UserNo)
+	,PADate
+		DATE
+		NOT NULL);		
+		
+		
+	
+	
+
 	
 		
 			
