57698df5aee28cf7cdf0c42d5d3738b21131b32c
HIS <niu280783872@qq.com>
2017-10-20 19:18:54 +0800

    完善

--- 数据库建立/king_4（建库代码）改+插入.sql
+++ 数据库建立/king_4（建库代码）改+插入.sql
@@ -20,6 +20,15 @@ CREATE TABLE tb_MedicineType
 	,Type 
 		VARCHAR(50)
 		NOT NULL);
+INSERT tb_Medicine
+	(TypeNo
+	,Type)
+	VALUES
+	('1','抗胆碱药')
+	,('2','抗肾上腺素药')
+	,('3','镇静催眠药')
+	,('4','抗高血压药');
+
 		
 IF OBJECT_ID ('tb_Medicine') IS NOT NULL 
 		DROP TABLE tb_Medicine ;
@@ -56,6 +65,30 @@ CREATE TABLE tb_Medicine
 		INT 
 		NOT NULL 
 	);
+INSERT tb_Medicine
+	(MedicineNo
+	,MedicineName
+	,TypeNo
+	,MFuntion
+	,PackingUnit
+	,BasicDose
+	,DoseUnit
+	,Guarantee)
+	VALUES
+	('1','硫酸阿托品片','1','内脏绞痛，有机磷酸酯类中毒','瓶','1.5','mg','48个月')
+,('2','氢溴酸东莨菪碱片','1','麻醉前给药，感染性休克','瓶','1','mg','24个月')
+,('3','溴甲贝那替秦片','1','胃及十二指肠溃疡，胃酸过多症','瓶','45','mg','24个月')
+,('4','甲磺酸酚妥拉明片','2','用于勃起功能障碍的治疗','瓶','40','mg','48个月')
+,('5','盐酸哌唑嗪片','2','轻，中度高血压','瓶','2','mg','36个月')
+,('6','盐酸普萘洛尔片?','2','高血压，心肌梗死','瓶','120','mg','36个月')
+,('7','地西泮片','3','抗焦虑，镇静，催眠','瓶','5','mg','48个月')
+,('8','阿普唑仑片','3','抗焦虑，抗抑郁','瓶','1.2','mg','36个月')
+,('9','三唑仑片','3','镇静催眠','瓶','0.5','mg','36个月')
+,('10','氢氯噻嗪片','4','高血压，水肿性疾病','瓶','30','mg','24个月')
+,('11','硝苯地平片','4','心绞痛，高血压','瓶','50','mg','36个月')
+,('12','氯沙坦钾片','4','高血压','瓶','50','mg','36个月');
+
+
 		
 IF OBJECT_ID ('tb_User') IS NOT NULL 
 		DROP TABLE tb_User ;
@@ -72,6 +105,15 @@ CREATE TABLE tb_User
 	,PassCode
 		VARBINARY(MAX)
 		NOT NULL );
+INSERT tb_User
+	(UserNo
+	,UserName
+	,PassCode)
+	VALUES
+('1','admin1','123')
+,('2','admin2','123');
+
+
 		
 IF OBJECT_ID ('tb_InWarehouse') IS NOT NULL 
 		DROP TABLE tb_InWarehouse;
@@ -103,6 +145,17 @@ CREATE TABLE tb_InWarehouse
 	,InPrice
 	    FLOAT 
 	    NOT NULL );
+INSERT tb_InWarehouse
+(InNo
+,InDate
+,UserNo
+,MedicineName
+,InAmount
+,InPrice)
+VALUES
+('171016001','2017.10.16','1','地西泮片','300','25')
+,('171016002','2017.10.16','2','硫酸阿托品片','300','15');
+
 	    
 IF OBJECT_ID ('tb_Pharmacy') IS NOT NULL 
 		DROP TABLE tb_Pharmacy;
@@ -116,6 +169,12 @@ CREATE TABLE tb_Pharmacy
 	,Type 
 		VARCHAR(10)
 		NOT NULL );
+INSERT tb_Pharmacy
+(PhNo
+,Type)
+VALUES
+('01','门诊药房')
+,('02','住院药房');
 
 IF OBJECT_ID ('tb_OutWarehouse') IS NOT NULL 
 		DROP TABLE tb_OutWarehouse;
@@ -126,11 +185,11 @@ CREATE TABLE tb_OutWarehouse
 		NOT NULL 
 		CONSTRAINT pk_Pharmary_No
 			PRIMARY KEY(OutNo)
-	,PharmacyNo
+	,PhNo
 		INT 
 		NOT NULL 
-		CONSTRAINT fk_OutWarehouse_PharmacyNo
-			FOREIGN KEY(PharmacyNo)
+		CONSTRAINT fk_OutWarehouse_PhNo
+			FOREIGN KEY(PhNo)
 			REFERENCES tb_Pharmacy(PhNo)	
 	,MedicineNo
 		INT 
@@ -150,6 +209,17 @@ CREATE TABLE tb_OutWarehouse
 		CONSTRAINT fk_OutWarehouse_UserNo
 			FOREIGN KEY(UserNo)
 			REFERENCES tb_Pharmacy(UserNo));
+INSERT tb_OutWarehouse
+(OutNo
+,PhNo
+,MedicineNo
+,OutAmount
+,OutDate
+,UserNo)
+VALUES
+('171016001','1','1','50','1996.10.16','1')
+,('171016002','2','2','30','1996.10.16','1');
+
 		
 	
 IF OBJECT_ID ('tb_Checker') IS NOT NULL 
@@ -165,6 +235,13 @@ CREATE TABLE tb_Checker
 		VARCHAR(20)
 		NOT NULL
 	);
+INSERT tb_Checker
+(ChNo
+,ChName)
+VALUES
+('1','李白')
+,('2','黄忠');
+
 		
 IF OBJECT_ID ('tb_Stock') IS NOT NULL 
 		DROP TABLE tb_Stock;
@@ -208,10 +285,25 @@ CREATE TABLE tb_Stock
 	,DtFinal
 		DATE
 		NOT NULL);
-IF OBJECT_ID('tb_Priceadjust') IS NOT NULL
+INSERT tb_Stock
+(StockNo
+,MedicineNo
+,StocckAmount
+,InDate
+,Firm
+,InPrice
+,OutPrice
+,ChNo
+,DtProduct
+,DtFinal)
+VALUES
+('1','1','1000','2017.10.16','杭州默沙东制药有限公司','30','40','1','2017.09.30','2019.09.30')
+,('2','2','800','2017.10.16','浙江六和制药厂','25','45','1','2017.09.30','2018.09.30');
+
+IF OBJECT_ID('tb_PriceAdjust') IS NOT NULL
 	DROP TABLE tb_priceadjust;
 create TABLE tb_priceadjust
-	(PaNo
+	(PANo
 		INT
 		NOT NULL
 		CONSTRAINT pk_priceadjust_PrNo
@@ -228,7 +320,7 @@ create TABLE tb_priceadjust
 		CONSTRAINT fk_Priceadjust_UserNo
 			FOREIGN KEY(UserNo)
 			REFERENCES tb_User(UserNo)
-	,PaDate
+	,PADate
 		DATE
 		NOT NUL
 	,BeforePrice
@@ -236,7 +328,18 @@ create TABLE tb_priceadjust
 		NOT NULL
 	,BehindPrice
 		FLOAT
-		NOT NULL);		
+		NOT NULL);
+INSERT tb_PriceAdjust	
+(PANo
+,MedicineNo
+,UserNo
+,PADate
+,BeforePrice
+,BehinfPrice)
+VALUES
+('1','1','1','2017.10.16','40','39')
+,('2','2','1','2017.10.16','45','42');
+	
 		
 		
 	
